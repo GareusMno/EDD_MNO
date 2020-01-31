90a696f670c580cc7443e457426c6b76b1cfd937
alumne <mnosuperior@gmail.com>
2020-01-31 10:00:06 +0100

    unidad 4 make

---
+++ EDD_MNO/EDD_MNO-Unidad-2/Ejercicio1.md
@@ -0,0 +1,8 @@
+#VSCODE 
+
+##Instalación de extensiones
+
+![Primera parte](1.PNG)
+![Segunda parte](2.PNG)
+![Tercera parte](3.PNG)
+![Cuarta parte](4.PNG)
---
+++ EDD_MNO/Unidad4/Make/calc.c
@@ -0,0 +1,20 @@
+int suma(int op1,int op2){
+	return (op1+op2);
+	}
+int resta(int op1,int op2){
+	return (op1-op2);
+	}
+int multiplica(int op1,int op2){
+	return (op1*op2);
+	}
+int divideix(int op1,int op2){
+	return (op1/op2);
+	}
+int major(int op1, int op2,int op3){
+	if (op1>op2){
+		op3=op1;
+	}else{
+		op3=op2;
+	}
+	return (op3);
+}
